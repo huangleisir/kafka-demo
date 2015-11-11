6f073c9dc0e81372b6e2ac26c65b1ceabee4a1e3
javabilities <meinholz@gmail.com>
2015-11-11 08:51:22 -0500

    Added payload POJO's for Jackson data.

---
+++ producer/src/main/java/com/javabilities/producer/domain/MessagePayload.java
@@ -0,0 +1,22 @@
+package com.javabilities.producer.domain;
+
+public class MessagePayload {
+    String topic;
+    String message;
+
+    public String getTopic() {
+        return topic;
+    }
+
+    public void setTopic(String topic) {
+        this.topic = topic;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+}
